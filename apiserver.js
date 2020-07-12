401dcbce5bb5a1e1474a130957240fdeb0d19958
ssoprey <sunil@soprey.local>
2020-07-12 04:41:49 +0000

    add post

--- apiserver.js
+++ apiserver.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setting for Hyperledger Fabric
 const { Gateway,Wallets } = require('fabric-network');
@@ -39,8 +40,9 @@ app.get('/api/queryallcars', async function (req, res) {
         await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
+        const network = await gateway.getNetwork('healthcare');
 
+        console.log(network);
         // Get the contract from the network.
         const contract = network.getContract('fabcar');
 
@@ -84,7 +86,7 @@ app.get('/api/query/:car_index', async function (req, res) {
         await gateway.connect(ccpPath, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: false } });
 
         // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
+        const network = await gateway.getNetwork('healthcare');
 
         // Get the contract from the network.
         const contract = network.getContract('fabcar');
@@ -106,32 +108,43 @@ app.post('/api/addcar/', async function (req, res) {
     try {
 
         // Create a new file system based wallet for managing identities.
+        //const walletPath = path.join(process.cwd(), 'wallet');
+        //const wallet = new FileSystemWallet(walletPath);
+        //console.log(`Wallet path: ${walletPath}`);
+
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = new FileSystemWallet(walletPath);
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('appUser');
-        if (!userExists) {
+        const identity = await wallet.get('appUser');
+        if (!identity) {
             console.log('An identity for the user "appUser" does not exist in the wallet');
             console.log('Run the registerUser.js application before retrying');
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
+        //const gateway = new Gateway();
+        //await gateway.connect(ccpPath, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: false } });
         const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: false } });
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
+        const network = await gateway.getNetwork('healthcare');
 
         // Get the contract from the network.
         const contract = network.getContract('fabcar');
 
+	console.log(req.body);
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('createCar', req.body.carid, req.body.make, req.body.model, req.body.colour, req.body.owner);
+        await contract.submitTransaction('createCar', req.body.carid, req.body.make,
+			req.body.model, req.body.colour, req.body.owner);
         console.log('Transaction has been submitted');
         res.send('Transaction has been submitted');
 
@@ -148,9 +161,17 @@ app.put('/api/changeowner/:car_index', async function (req, res) {
     try {
 
         // Create a new file system based wallet for managing identities.
+        //const walletPath = path.join(process.cwd(), 'wallet');
+        //const wallet = new FileSystemWallet(walletPath);
+        //console.log(`Wallet path: ${walletPath}`);
+
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = new FileSystemWallet(walletPath);
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
         console.log(`Wallet path: ${walletPath}`);
+
 
         // Check to see if we've already enrolled the user.
         const userExists = await wallet.exists('appUser');
@@ -165,7 +186,7 @@ app.put('/api/changeowner/:car_index', async function (req, res) {
         await gateway.connect(ccpPath, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: false } });
 
         // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
+        const network = await gateway.getNetwork('healthcare');
 
         // Get the contract from the network.
         const contract = network.getContract('fabcar');
